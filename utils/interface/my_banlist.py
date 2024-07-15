9a61f1616693599122219e0834ff93804b49a873
Alekzum <68947902+Alekzum@users.noreply.github.com>
2024-07-15 15:21:22 +0400

    use json, not sqlite3 (for compatibility, on some systems sqlite3 don't work normal)

--- utils/interface/my_banlist.py
+++ utils/interface/my_banlist.py
@@ -1,50 +1,110 @@
+from typing import TypedDict
 import pathlib
-import sqlite3
+import json
+import time
 
 
-BINDS_PATH = pathlib.Path("data/bocklist.db")
-BINDS_FILENAME = str(BINDS_PATH)
-TABLE_NAME = "Users"
+BANS_PATH = pathlib.Path("data/banlist.json")
+BANS_FILENAME = str(BANS_PATH)
 SPLITTER = ","
 
 
-with sqlite3.connect(BINDS_FILENAME) as con:
-    cur = con.cursor()
-    cur.execute(f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME}(
-    uid INTEGER UNIQUE,
-    banned INTEGER,
-    bannedReason TEXT
-)""")
+# uid INTEGER UNIQUE,
+# banned INTEGER,
+# bannedReason TEXT
+
+
+class FIELDS(TypedDict):
+    banned: bool
+    bannedReason: None | str
+
+_fileBusy = False
+_update_time = 0.01
+
+
+def wait_unlock():
+    global _fileBusy
+    while _fileBusy:
+        time.sleep(_update_time)
+
+
+def lock_file():
+    global _fileBusy
+    _fileBusy = True
+
+
+def unlock_file():
+    global _fileBusy
+    _fileBusy = False
+
+
+def WithLocking(func):
+    def inner(*args, **kwargs):
+        wait_unlock()
+        lock_file()
+        result = func(*args, **kwargs)
+        unlock_file()
+        return result
+    return inner
+
+
+@WithLocking
+def load_raw() -> dict[int, FIELDS]:
+    try:
+        with open(BANS_FILENAME, encoding="utf-8") as file:
+            raw = json.load(file)
+    except (json.JSONDecodeError, FileNotFoundError):
+        raw = {}
+    return raw
+
+
+@WithLocking
+def save_raw(raw):
+    with open(BANS_FILENAME, "w", encoding="utf-8") as file:
+        json.dump(raw, file, ensure_ascii=False, separators=(',', ':'))
 
 
 def banUser(uid: int, reason: str):
-    with sqlite3.connect(BINDS_FILENAME) as con:
-        cur = con.cursor()
-        cur.execute(f"INSERT INTO {TABLE_NAME} (uid, banned, bannedReason) VALUES (?, 1, ?)", (uid, reason))
-        con.commit()
+    raw = load_raw()
+    raw[uid] = dict(banned=True, bannedReason=reason)
+    save_raw(raw)
+#     with sqlite3.connect(BANS_FILENAME) as con:
+#         cur = con.cursor()
+#         cur.execute(f"INSERT INTO {TABLE_NAME} (uid, banned, bannedReason) VALUES (?, 1, ?)", (uid, reason))
+#         con.commit()
 
 
 def unbanUser(uid: int):
-    with sqlite3.connect(BINDS_FILENAME) as con:
-        cur = con.cursor()
-        cur.execute(f"DELETE FROM {TABLE_NAME} WHERE uid=?", (uid, ))
-        con.commit()
+    raw = load_raw()
+    raw[uid] = dict(banned=False, bannedReason=None)
+    save_raw(raw)
+#     with sqlite3.connect(BANS_FILENAME) as con:
+#         cur = con.cursor()
+#         cur.execute(f"DELETE FROM {TABLE_NAME} WHERE uid=?", (uid, ))
+#         con.commit()
 
 
 def getBannedUsers() -> list[int] | list:
-    with sqlite3.connect(BINDS_FILENAME) as con:
-        cur = con.cursor()
-        cur.execute(f"SELECT uid FROM {TABLE_NAME} WHERE banned=1", ())
-        results = cur.fetchall()
-    return results or []
+    raw = load_raw()
+    result = [k for (k, v) in raw.items() if v.get("banned")]
+    return result
+    
+#     with sqlite3.connect(BANS_FILENAME) as con:
+#         cur = con.cursor()
+#         cur.execute(f"SELECT uid FROM {TABLE_NAME} WHERE banned=1", ())
+#         results = cur.fetchall()
+#     return results or []
 
 
 def getBannedReason(uid: int) -> str:
-    with sqlite3.connect(BINDS_FILENAME) as con:
-        cur = con.cursor()
-        cur.execute(f"SELECT bannedReason FROM {TABLE_NAME} WHERE uid=?", (uid, ))
-        result = cur.fetchone()
-    return result or "*не указано*"
+    raw = load_raw()
+    result = raw.get(uid).get("bannedReason", "*не указано*")
+    return result
+#     with sqlite3.connect(BANS_FILENAME) as con:
+#         cur = con.cursor()
+#         cur.execute(f"SELECT bannedReason FROM {TABLE_NAME} WHERE uid=?", (uid, ))
+#         result = cur.fetchone()
+#     return result or "*не указано*"
 
 
 def isBanned(uid: int) -> bool:
