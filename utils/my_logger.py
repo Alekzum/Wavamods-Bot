5542fe75ec32032724f08a4cd1692c37242b991b
Alekzum <68947902+Alekzum@users.noreply.github.com>
2024-07-21 15:58:08 +0400

    Logging to console now have toggle

--- utils/my_logger.py
+++ utils/my_logger.py
@@ -3,6 +3,14 @@ from typing import Any
 import logging
 
 
+LOG_TO_CONSOLE = False
+
+LOGGING_COOLDOWN = 3
+FORMAT = '{asctime} - [{levelname}] {filename}:{funcName}:{lineno} {name} - {message}'
+INFO = logging.INFO
+ERROR = logging.ERROR
+
+
 class CooldownFilter(logging.Filter):
     """Do not print same line if time after previous line less or equal <COOLDOWN> seconds. Defaults to 5 seconds"""
     def __init__(self, name='', cooldown=5):
@@ -28,22 +36,17 @@ class CooldownFilter(logging.Filter):
             return False
 
 
-LOGGING_COOLDOWN = 3
-FORMAT = '{asctime} - [{levelname}] {filename}:{funcName}:{lineno} {name} - {message}'
-INFO = logging.INFO
-ERROR = logging.ERROR
+fileHandler = logging.FileHandler(filename="log.log")
+fileHandler.addFilter(CooldownFilter())
+fileHandler.setLevel(INFO)
 
+streamHandler = logging.StreamHandler()
+streamHandler.addFilter(CooldownFilter())
+streamHandler.setLevel(INFO)
 
-# fileHandler = logging.FileHandler(filename="log.log")
-# fileHandler.addFilter(CooldownFilter())
-# fileHandler.setLevel(INFO)
+handlers = [fileHandler, streamHandler] if LOG_TO_CONSOLE else [fileHandler]
 
-# streamHandler = logging.StreamHandler()
-# streamHandler.addFilter(CooldownFilter())
-# streamHandler.setLevel(INFO)
-
-# logging.basicConfig(format=FORMAT, level=INFO, style="{", handlers=[fileHandler, streamHandler])
-logging.basicConfig(format=FORMAT, level=INFO, style="{")
+logging.basicConfig(format=FORMAT, level=INFO, style="{", handlers=handlers)
 
 logging.getLogger("aiogram").setLevel(INFO)
 logging.getLogger("aiohttp").setLevel(INFO)
